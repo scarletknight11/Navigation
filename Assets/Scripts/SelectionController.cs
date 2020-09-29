7c8cd0df56c081f812dd1d9ed0466d37a45cdc6c
Sanjit Singh <sanjitpsingh@yahoo.com>
2020-09-28 22:10:29 -0400

    got object selecton working. now only object i select will move. just need to make mulitple selections work

---
+++ Assets/Scripts/SelectionController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionController : MonoBehaviour {
+
+    [HideInInspector] public GameObject currentlySelectedCharacter;
+
+    public void SelectCharacter(GameObject charToSelect) {
+        currentlySelectedCharacter = charToSelect;
+    }
+
+    public void DeselectCharacter() {
+        currentlySelectedCharacter.GetComponent<MovePlayer>().DeselectCharacter();
+        currentlySelectedCharacter = null;
+    }
+
+}
