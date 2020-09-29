94a2daba20530fc398d8776e7463ff8383ac088d
Sanjit Singh <sanjitpsingh@yahoo.com>
2020-09-29 00:12:25 -0400

    Added navmesh obstacle object you can move with arrows. selecting agent works but moving selected agent only 85% of it works. if I select multiple only the first agent selected will move first

---
+++ Assets/Scripts/movecube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class movecube : MonoBehaviour {
+
+    public float Speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Destination();
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.position += Vector3.back * Time.deltaTime;
+        }
+
+    }
+
+    private void Destination()
+    {
+        transform.Translate(Input.GetAxis("Horizontal")* Time.deltaTime, 0f, Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
